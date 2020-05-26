541cba20e5cd0d2462ab74e0430a91b73fa441fd
000258 <zhangguozhong@fcbox.com>
2020-05-26 15:41:37 +0800

    version 0.0.8

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -12,9 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // ToastView Sample
+//        ToastView.show("bring your ideas to life", inView: self.view)
     }
-
-
 }
 
